df49382972b35d24bd717931fdefb13ee7bcd12c
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2025-02-18 11:35:32 +0100

    Add project files.

---
+++ A6-PolovniAutomobiliA/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace A6_PolovniAutomobiliA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonIzadji = new System.Windows.Forms.Button();
+            this.buttonIzmeni = new System.Windows.Forms.Button();
+            this.textBoxSifra = new System.Windows.Forms.TextBox();
+            this.textBoxNaziv = new System.Windows.Forms.TextBox();
+            this.comboBoxProizvodjac = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(792, 421);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Uputstvo";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Broj raspolozivih vozila";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonSearch);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.buttonIzadji);
+            this.tabPage1.Controls.Add(this.buttonIzmeni);
+            this.tabPage1.Controls.Add(this.textBoxSifra);
+            this.tabPage1.Controls.Add(this.textBoxNaziv);
+            this.tabPage1.Controls.Add(this.comboBoxProizvodjac);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Modeli automobila";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackgroundImage = global::A6_PolovniAutomobiliA.Properties.Resources.search;
+            this.buttonSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonSearch.Location = new System.Drawing.Point(721, 21);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(49, 48);
+            this.buttonSearch.TabIndex = 21;
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(400, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(363, 308);
+            this.listBox1.TabIndex = 20;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // buttonIzadji
+            // 
+            this.buttonIzadji.Location = new System.Drawing.Point(148, 221);
+            this.buttonIzadji.Name = "buttonIzadji";
+            this.buttonIzadji.Size = new System.Drawing.Size(151, 36);
+            this.buttonIzadji.TabIndex = 19;
+            this.buttonIzadji.Text = "Izadji";
+            this.buttonIzadji.UseVisualStyleBackColor = true;
+            // 
+            // buttonIzmeni
+            // 
+            this.buttonIzmeni.Location = new System.Drawing.Point(148, 163);
+            this.buttonIzmeni.Name = "buttonIzmeni";
+            this.buttonIzmeni.Size = new System.Drawing.Size(151, 36);
+            this.buttonIzmeni.TabIndex = 18;
+            this.buttonIzmeni.Text = "Izmeni";
+            this.buttonIzmeni.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSifra
+            // 
+            this.textBoxSifra.Location = new System.Drawing.Point(606, 37);
+            this.textBoxSifra.Name = "textBoxSifra";
+            this.textBoxSifra.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSifra.TabIndex = 17;
+            // 
+            // textBoxNaziv
+            // 
+            this.textBoxNaziv.Location = new System.Drawing.Point(121, 78);
+            this.textBoxNaziv.Name = "textBoxNaziv";
+            this.textBoxNaziv.Size = new System.Drawing.Size(178, 22);
+            this.textBoxNaziv.TabIndex = 16;
+            // 
+            // comboBoxProizvodjac
+            // 
+            this.comboBoxProizvodjac.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxProizvodjac.FormattingEnabled = true;
+            this.comboBoxProizvodjac.Location = new System.Drawing.Point(121, 37);
+            this.comboBoxProizvodjac.Name = "comboBoxProizvodjac";
+            this.comboBoxProizvodjac.Size = new System.Drawing.Size(178, 24);
+            this.comboBoxProizvodjac.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(414, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(552, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Sifra";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Naziv";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Proizvodjac";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Model";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonIzadji;
+        private System.Windows.Forms.Button buttonIzmeni;
+        private System.Windows.Forms.TextBox textBoxSifra;
+        private System.Windows.Forms.TextBox textBoxNaziv;
+        private System.Windows.Forms.ComboBox comboBoxProizvodjac;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ A6-PolovniAutomobiliA/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A6_PolovniAutomobiliA
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection
+            (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\A6.mdf;Integrated Security=True");
+        DataTable dtm = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopuniComboProizvodjaca();
+            PopuniListuModela();
+        }
+        private void PopuniComboProizvodjaca()
+        {
+            string upit = "SELECT * FROM Proizvodjac";
+            SqlCommand cmd = new SqlCommand(upit, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dtp = new DataTable();
+            try
+            {
+                da.Fill(dtp);
+                comboBoxProizvodjac.DataSource=dtp;
+                comboBoxProizvodjac.DisplayMember = "Naziv";
+                comboBoxProizvodjac.ValueMember = "ProizvodjacID";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void PopuniListuModela()
+        {
+            string upit = "SELECT m.ModelID, m.Naziv, p.Naziv " +
+                "FROM Model AS m, Proizvodjac AS p " +
+                "WHERE m.ProizvodjacID=p.ProizvodjacID";
+            SqlCommand cmd = new SqlCommand(upit, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            try
+            {
+                da.Fill(dtm);
+                foreach (DataRow dr in dtm.Rows)
+                {
+                    string red = string.Format("{0,-6}{1}, {2}",
+                        dr[0], dr[1], dr[2]);
+                    listBox1.Items.Add(red);    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                int selind = listBox1.SelectedIndex;
+                textBoxSifra.Text = dtm.Rows[selind][0].ToString();
+                textBoxNaziv.Text = dtm.Rows[selind][1].ToString();
+                comboBoxProizvodjac.Text = dtm.Rows[selind][2].ToString();
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            int idm;
+            if (int.TryParse(textBoxSifra.Text, out idm))
+            {
+                for (int i = 0; i < dtm.Rows.Count; i++)
+                {
+                    if (idm == (int)dtm.Rows[i][0])
+                    {
+                        listBox1.SelectedIndex = i;
+                        return;
+                    }
+                }
+                MessageBox.Show("Model sa šifrom " + idm + " ne postoji!");
+            }
+            else
+            {
+                MessageBox.Show("Unesite celobrojnu šifru!");
+            }
+            textBoxSifra.Focus();
+            textBoxSifra.SelectAll();
+        }
+    }
+}
